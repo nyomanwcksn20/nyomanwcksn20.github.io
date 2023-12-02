353eb3976291f9b9117e293595b14580fc37eba3
nyomanwcksn20 <nyomanello42@gmail.com>
2023-12-02 13:37:58 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function highlightProject(element) {
+    element.style.backgroundColor = "#e6e6e6";
+}
+
+function unhighlightProject(element) {
+    element.style.backgroundColor = "";
+}
+
+function goToIndex() {
+    window.location.href = "index.html";
+}
